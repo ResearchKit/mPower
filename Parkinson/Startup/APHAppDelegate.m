b466b5e58f42ef2c1689ede1e7d64d75f04b029f
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-06-17 14:42:12 -0700

    Replace Date Component Calculation with  timeIntervalSinceDate  Method
    Signed-off-by: Henry McGilton <henry.mcgilton@ymedialabs.com>

--- Parkinson/Startup/APHAppDelegate.m
+++ Parkinson/Startup/APHAppDelegate.m
@@ -585,11 +585,8 @@ static NSDate *DetermineConsentDate(id object)
             quantitySource = determineQuantitySource(quantitySource);
             
             // Get the difference in seconds between the start and end date for the sample
-            NSDateComponents* secondsSpentInBedOrAsleep = [[NSCalendar currentCalendar] components:NSCalendarUnitSecond
-                                                                                          fromDate:catSample.startDate
-                                                                                            toDate:catSample.endDate
-                                                                                           options:NSCalendarWrapComponents];
-            NSString*           quantityValue   = [NSString stringWithFormat:@"%ld", (long)secondsSpentInBedOrAsleep.second];
+            NSTimeInterval secondsSpentInBedOrAsleep = [catSample.endDate timeIntervalSinceDate:catSample.startDate];
+            NSString*           quantityValue   = [NSString stringWithFormat:@"%ld", (long)secondsSpentInBedOrAsleep];
             
             stringToWrite = [NSString stringWithFormat:@"%@,%@,%@,%@,%@,%@,%@\n",
                              startDateTime,
